356ecb7657010a692737dd9cdcd48c601d61ec35
tarunkhatana1987@gmail.com <tarunkhatana1987@gmail.com>
2019-01-19 23:48:50 +0530

    Added configuration solution

---
+++ MyApp.Configuration/MyAppContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+using MyApp.Entity.Poco;
+using MyApp.Configuration.Mapping;
+
+namespace MyApp.Configuration
+{
+    public class MyAppContext : DbContext
+    {
+        public MyAppContext()
+            : base("MyAppContext")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+        }
+
+        //public MyAppContext(string connectionString)
+        //    : base(connectionString)
+        //{
+        //    this.Configuration.LazyLoadingEnabled = false;
+        //    this.Configuration.ProxyCreationEnabled = false;
+        //}
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new UserFormMap());
+        }
+    }
+}
